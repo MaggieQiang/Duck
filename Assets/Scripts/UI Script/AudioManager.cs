9eb3dd6823ef15408bb759f47bbe281fbd4cbf35
raneema607 <raneema607@g.ucla.edu>
2025-10-30 14:47:07 -0700

    Quit Button Attachment

--- Assets/Scripts/UI Script/AudioManager.cs
+++ Assets/Scripts/UI Script/AudioManager.cs
@@ -3,23 +3,19 @@ using UnityEngine.Audio;
 public class Audio : MonoBehaviour
 {
 
-    [SerializeField] AudioClip slashSound;
+
     [SerializeField] AudioSource defaultSource;
     [SerializeField] AudioMixer audioMixer;
-    [SerializeField] AudioMixerSnapshot cutsceneShot;
+
 
     private void Start()
     {
-        cutsceneShot.TransitionTo(5f);
+        
     }
 
-    public void PlayJump()
-    {
-        defaultSource.PlayOneShot(slashSound);
-    }
-    
+
     public void ChangeVolume(float volume)
     {
-        audioMixer.SetFloat("MasterVolume", volume);
+        defaultSource.volume = volume;
     }
 }
--- Assets/Scripts/UI Script/TitleScreen.cs
+++ Assets/Scripts/UI Script/TitleScreen.cs
@@ -6,7 +6,7 @@ public class TitleScreen : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //
     }
 
     // Update is called once per frame
@@ -25,4 +25,10 @@ public class TitleScreen : MonoBehaviour
         Application.Quit();
         Debug.Log("quit");
     }
+
+    public void GoToLastScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
 }
