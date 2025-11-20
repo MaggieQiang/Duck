12b6dab826d43e5845d158c75b65615f3e268086
MaggieQiang <maggieqiang717@gmail.com>
2025-11-20 12:31:34 -0800

    SharkAwareness

---
+++ Assets/Scripts/Shark/SharkMovement.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class SharkMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed;
+    private Rigidbody2D rd;
+    private PlayerAwarenessController _playerAwarenessController;
+    private Vector2 _targetDirection;
+    
+    private void Awake()
+    {
+        rd = GetComponent<Rigidbody2D>();
+        _playerAwarenessController = GetComponent<PlayerAwarenessController>();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateTargetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+    }
+
+    private void UpdateTargetDirection()
+    {
+        if (_playerAwarenessController.AwareOfPlayer)
+        {
+            _targetDirection = _playerAwarenessController.DirectionToPlayer;
+        }
+        else
+        {
+            _targetDirection = Vector2.zero;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        if (_targetDirection == Vector2.zero)
+        {
+            return;
+        }
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+        rd.SetRotation(rotation);
+    }
+
+    private void SetVelocity()
+    {
+        if (_targetDirection == Vector2.zero)
+        {
+            rd.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            rd.linearVelocity = transform.up * _speed;
+        }
+    }
+}
